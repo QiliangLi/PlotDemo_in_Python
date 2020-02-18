e2212400efb413b76ddbae0b8b2bb9decd763e88
Qiliang Li <799069561@qq.com>
2020-02-18 23:39:07 +0800

    plot first batch

---
+++ FirstBatch.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+avgRatio = []
+randRatio = []
+
+avgFile=pd.read_csv(r"F:\Coding\Java\SimulateSR\result\201-2001-avg-result-K=6-M=3-gains=100.0-repeatTimes=5.csv")
+for ratio in avgFile["RatioAvg"]:
+    avgRatio.append(ratio)
+
+randFile=pd.read_csv(r"F:\Coding\Java\SimulateSR\result\201-2001-rand-avg-result-K=6-M=3-gains=100.0-repeatTimes=5.csv")
+for ratio in randFile["RatioAvg"]:
+    randRatio.append(ratio)
+
+fig, ax = plt.subplots(figsize=(10, 4))
+
+plt.xlabel('Node Number')
+plt.ylabel('Prallel Rate Ratio')
+
+"""set interval for y label"""
+xticks=range(201, 2002, 100)
+ax.set_xticks(xticks)
+
+"""set min and max value for axes"""
+ax.set_ylim([0, 1.1])
+ax.set_xlim([201, 2001])
+figTitle='K=6, M=3, Gains=100, RepeatTimes=5'
+ax.set_title(figTitle)
+
+x = [i for i in range(201, 2002, 100)]
+plt.plot(x, avgRatio, "x-", label="SR Ratio")
+plt.plot(x, randRatio, "+-", label="Rand Ratio")
+
+"""open the grid"""
+plt.grid(False)
+
+#plt.legend(bbox_to_anchor=(1.0, 1), loc=4, borderaxespad=0.)
+plt.legend(loc=4)
+
+plt.savefig("Big Case First Batch "+figTitle)
+
+plt.show()
